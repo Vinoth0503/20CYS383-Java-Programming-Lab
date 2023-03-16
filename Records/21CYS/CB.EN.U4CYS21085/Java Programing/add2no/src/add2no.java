d738b9602f443a965e95057a001cdaee4e89ed5c
Vinoth Kumar C <92920137+Vinoth0503@users.noreply.github.com>
2023-03-16 15:49:36 +0530

    Updated programs

---
+++ Records/21CYS/CB.EN.U4CYS21085/Java Programing/add2no/src/add2no.java
@@ -0,0 +1,11 @@
+public class add2no
+{
+    public static void main(String args[]) {
+        int n1 = 10;
+        int n2 = 10;
+        int sum;
+        sum = n1 + n2;
+        System.out.println("The sum of 2 numbers is: " + sum);
+    }
+
+}
---
+++ Records/21CYS/CB.EN.U4CYS21085/Java Programing/gui_example/src/gui_example.java
@@ -0,0 +1,5 @@
+public class gui_example {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
