a63806221e33bdac901697def74f51bae39ce51c
Vinoth Kumar C <92920137+Vinoth0503@users.noreply.github.com>
2023-03-20 16:23:34 +0530

    Uploaded

---
+++ Records/21CYS/CB.EN.U4CYS21085/Java/pattern.java
@@ -0,0 +1,15 @@
+package com.amrita.vinoth;
+public class pattern {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            System.out.println("* * * * * * ==================================");
+            System.out.println(" * * * * *  ===================================");
+        }
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+    }
+}
